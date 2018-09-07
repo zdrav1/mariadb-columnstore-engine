7fad891bd21f607cde153e0be0be8ad8d5a7ecab
David Hall <david.hall@mariadb.com>
2018-09-07 10:46:50 -0500

    MCOL-1698 add CREATE AGGREGATE FUNCTION distinct_count to startup

--- dbcon/mysql/install_calpont_mysql.sh
+++ dbcon/mysql/install_calpont_mysql.sh
@@ -85,6 +85,7 @@ CREATE FUNCTION idblocalpm RETURNS INTEGER soname 'libcalmysql.so';
 CREATE FUNCTION mcssystemready RETURNS INTEGER soname 'libcalmysql.so';
 CREATE FUNCTION mcssystemreadonly RETURNS INTEGER soname 'libcalmysql.so';
 CREATE AGGREGATE FUNCTION regr_avgx RETURNS REAL soname 'libudf_mysql.so';
+CREATE AGGREGATE FUNCTION distinct_count returns INT soname 'libudf_mysql.so';
 
 CREATE DATABASE IF NOT EXISTS infinidb_vtable;
 CREATE DATABASE IF NOT EXISTS infinidb_querystats;
